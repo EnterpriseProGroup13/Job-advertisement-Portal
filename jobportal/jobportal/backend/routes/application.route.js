36e73be7c8ecd66dffaa7c6f7c8da629dce0aa86
BLACCSK <91878683+BLACCSK@users.noreply.github.com>
2025-04-09 10:25:09 -0700

    Update

--- jobportal/jobportal/backend/routes/application.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { applyJob, getApplicationsForJob } from '../controllers/application.controller.js';
-import { protect } from '../middlewares/auth.middleware.js';
-const router = express.Router();
-
-router.post('/', protect, applyJob);
-router.get('/:jobId', protect, getApplicationsForJob);
-
-export default router;
