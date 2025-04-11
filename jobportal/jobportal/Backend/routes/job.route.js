88f1fffb30b5280feec09a10151ff7d8814b76e3
SOPURU EGBOSI <91878683+BLACCSK@users.noreply.github.com>
2025-04-10 21:40:39 -0700

    Update job.route.js

--- jobportal/jobportal/Backend/routes/job.route.js
+++ jobportal/jobportal/Backend/routes/job.route.js
@@ -8,3 +8,12 @@ router.get('/', getJobs);
 router.get('/:id', getJobById);
 
 export default router;
+
+router.post("/", async (req, res) => {
+    try {
+      const job = await Job.create(req.body);
+      res.status(200).json(job);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
